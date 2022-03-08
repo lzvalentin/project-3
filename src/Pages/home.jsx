eb77c1074504a0308275c88bf1219eafb98c5039
Keara Cotter <kearacotter@gmail.com>
2022-03-08 13:32:03 -0800

    add basic html structure

---
+++ src/Pages/home.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Nav from '../components/Nav/Nav';
+import Rightbar from '../components/rightbar/Rightbar';
+import Sidebar from '../components/sidebar/Sidebar';
+import Feed from '../components/feed/Feed';
+import "./home.css";
+
+export default function Home() {
+  return (
+    <>
+      <Nav/>
+      <div className="homeContainer">
+      <Sidebar/>
+      <Feed/>
+      <Rightbar/>
+      </div>
+    </>
+    );
+}
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./nav.css";
+
+export default function nav() {
+  return (
+    <div className='navbarContainer'>
+        <div className="navLeft">
+          <span className="logo">Neighbors</span>
+        </div>
+    </div>
+  )
+}
