d3adcc642a2c73755c71295ad2bedbe9355d1551
Lorena <lzuniga1315@gmail.com>
2022-03-09 14:34:14 -0800

    wip login no design

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import Home from "./Pages/home";
+// import { BrowserRouter, Route } from 'react-router-dom';
+import Login from '../src/Pages/Login/login.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../src/Pages/Login/login.css'
 
 function App() {
-  return <Home/>;
+  return (
+
+    <>
+    <Login />
+      <Home />;
+      ;
+    </>
+  )
 }
 
 export default App;
