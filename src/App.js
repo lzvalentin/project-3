e2edf245c50d6c28aaee42a888c11cbcabdadc56
Karmjeet Nehal <karmnamol@gmail.com>
2022-03-09 12:38:22 -0800

    fixing merge conflicts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Contact from './components/Contact';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+    <Router>
+     
+        <div>
+          <h2>Welcome to My Neighbourhood </h2>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <ul className="navbar-nav mr-auto">
+            <li><Link to={'/'} className="nav-link"> Login </Link></li>
+            <li><Link to={'/contact'} className="nav-link">Contact</Link></li>
+            <li><Link to={'/signup'} className="nav-link">Signup</Link></li>
+          </ul>
+          </nav>
+          <hr />
+          <Routes>
+              <Route exact path='/' component={Login} />
+              <Route path='/contact' component={Contact} />
+              <Route path='/about' component={Signup} />
+              </Routes>
+        </div>
+        
+      </Router>
+    );
+  }
 }
 
 export default App;
