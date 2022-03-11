f85a9425b5ba0d72638d9380a32c12ec30af2696
Bernardo Dibildox <95992081+bdibil@users.noreply.github.com>
2022-03-11 14:20:10 -0800

    feed test

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,5 +1,5 @@
-// const BASEURL="http://localhost:3000"
-// // const BASEURL=""
+const BASEURL="http://localhost:3000"
+// const BASEURL=""
 
 // const API = {
 //     getTokenData: (token)=>{
@@ -31,5 +31,42 @@
 //         return  fetch(`${BASEURL}/api/users/${id}`).then(res=>res.json())
 //     }
 // }
+const API = {
+    getTokenData: (token)=>{
+        return fetch(`${BASEURL}/users`, {
+            headers: {
+              authorization: `Bearer ${token}`
+            }
+          })
+            .then(res => {
+                console.log(res)
+                res.json()
+            })
+    },
+    login: (email,password)=>{
+       return fetch(`${BASEURL}/users/login`, {
+           method: "POST",
+           body: JSON.stringify({
+               email: email,
+               password: password
+           }),
+           headers: {
+               "Content-Type": "application/json"
+           }
+       })
+            .then(res => {
+                console.log(res)
+                res.json()
+            })
 
-// export default API;
+    }
+    // getTanks:()=>{
+    //     return fetch(`${BASEURL}/api/tanks`)
+    //     .then(res => res.json())
+    // },
+    // getSingleTank:id=>{
+    //     return  fetch(`${BASEURL}/api/tanks/${id}`).then(res=>res.json())
+    // }
+}
+
+export default API;
