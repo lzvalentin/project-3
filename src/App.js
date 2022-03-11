5d50b1f47610935a28b5ec79ebc2aee6b5c37355
Keara Cotter <kearacotter@gmail.com>
2022-03-11 09:52:01 -0800

    user login functionality

--- src/App.js
+++ src/App.js
@@ -1,79 +1,33 @@
 import Home from "./Pages/home/home";
-// import { BrowserRouter, Route } from 'react-router-dom';
-import Login from '../src/Pages/Login/login.js';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import '../src/Pages/Login/login.css'
 import { useState, useEffect } from "react";
 import {BrowserRouter as Router, Routes, Route,} from "react-router-dom";
 import API from "./utils/API"
 import Nav from './components/Nav/Nav';
+import Login from "./Pages/Login/login"
 
 function App() {
-  // const [userEmail, setUserEmail] = useState("");
-  // const [userId, setUserId] = useState(0);
-  // const [token, setToken] = useState("");
-
-  // const [loginInfo, setLoginInfo] = useState({
-  //   email:"",
-  //   password:""
+  // const reqBody = {
+  //   "user":"joe@joe.joe",
+  //   "pwd":"password"
+  // }
+  // fetch("api/auth/login", {
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   method:"post",
+  //   body: JSON.stringify(reqBody),
   // })
-
-  // useEffect(() => {
-  //   const token = localStorage.getItem("token");
-  //   if (token) {
-  //     API.getTokenData(token)
-  //       .then(data => {
-  //         console.log(data);
-  //         setUserId(data.id);
-  //         setUserEmail(data.email);
-  //         setToken(token);
-  //       })
-  //       .catch(err => {
-  //         console.log(err);
-  //       });
-  //   }
-  // }, []);
-
-  // const logMeIn = (e) => {
-  //   e.preventDefault()
-  //   API.login(loginInfo.email,loginInfo.password)
-  //     .then(data => {
-  //       console.log(data);
-  //       setUserId(data.user.id);
-  //       setUserEmail(data.user.email);
-  //       setToken(data.token);
-  //       localStorage.setItem("token", data.token);
-  //     }).catch(err=>{
-  //       console.log(err);
-  //     });
-  // };
-
-  // const logMeOut = ()=>{
-  //   localStorage.removeItem("token");
-  //   setUserId(0);
-  //   setUserEmail("");
-  //   setToken("");
-  // }
-
-  // const handleInputChange = e=>{
-  //   console.log(e.target.name,e.target.value)
-  //   setLoginInfo({
-  //     ...loginInfo,
-  //     [e.target.name]:e.target.value
-  //   })
-  // }
-  // <Nav logMeOut={logMeOut} logMeIn={logMeIn} userEmail={userEmail} loginInfo={loginInfo} handleInputChange={handleInputChange}/>
+  // .then((response)=>{
+  //   return response.json()
+  // })
+  // .then((data)=>console.log(data));
 
   return (
-    <>
-      <Router>
-        <Nav/>
-        <Routes>
-          <Route path="/" element={<Login/>}></Route>
-          <Route path="/user" element={<Home/>}/>
-        </Routes>
-      </Router>
-    </>
-  );
+    <main>
+      <Nav />
+      <Login />
+    </main>
+  )
 }
 export default App;
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,35 +1,35 @@
-const BASEURL="http://localhost:3000"
-// const BASEURL=""
+// const BASEURL="http://localhost:3000"
+// // const BASEURL=""
 
-const API = {
-    getTokenData: (token)=>{
-        return fetch(`${BASEURL}/api/users`, {
-            headers: {
-              authorization: `Bearer ${token}`
-            }
-          })
-            .then(res => res.json())
-    },
-    login: (email,password)=>{
-       return fetch(`${BASEURL}/api/users/login`, {
-      method: "POST",
-      body: JSON.stringify({
-        email: email,
-        password:password
-      }),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-      .then(res => res.json())
-    },
-    getUsers:()=>{
-        return fetch(`${BASEURL}/api/users`)
-        .then(res => res.json())
-    },
-    getSingleUser:id=>{
-        return  fetch(`${BASEURL}/api/users/${id}`).then(res=>res.json())
-    }
-}
+// const API = {
+//     getTokenData: (token)=>{
+//         return fetch(`${BASEURL}/api/users`, {
+//             headers: {
+//               authorization: `Bearer ${token}`
+//             }
+//           })
+//             .then(res => res.json())
+//     },
+//     login: (email,password)=>{
+//        return fetch(`${BASEURL}/api/users/login`, {
+//       method: "POST",
+//       body: JSON.stringify({
+//         email: email,
+//         password:password
+//       }),
+//       headers: {
+//         "Content-Type": "application/json"
+//       }
+//     })
+//       .then(res => res.json())
+//     },
+//     getUsers:()=>{
+//         return fetch(`${BASEURL}/api/users`)
+//         .then(res => res.json())
+//     },
+//     getSingleUser:id=>{
+//         return  fetch(`${BASEURL}/api/users/${id}`).then(res=>res.json())
+//     }
+// }
 
-export default API;
+// export default API;
